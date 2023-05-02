78a6b47bf41344c09bd491806c655b1d3f9cc8c0
afonne-cid <delightinbusiness@gmail.com>
2023-05-02 18:51:03 +0100

    update: addapting to vercel's serverless functions

---
+++ analytics_dashboard/pages/api/index.js
@@ -0,0 +1,49 @@
+
+// // For localhost
+// const express = require('express');
+// const { GoogleAuth } = require('google-auth-library');
+// const { BetaAnalyticsDataClient } = require('@google-analytics/data');
+
+// const app = express();
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// const propertyId = process.env.PROPERTY_ID;
+// const port = process.env.PORT || 3001
+
+// app.listen(port, () => {
+//     console.log(`Server listening at http://localhost:${port}`)
+// })
+
+require('dotenv').config();
+
+const credentialsJson = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+const auth = new GoogleAuth({
+    credentials: credentialsJson,
+    scopes: ["https://www.googleapis.com/auth/analytics.readonly"],
+});
+
+
+module.exports = async (request, response) => {
+
+    try {
+
+        metrics = JSON.parse(request.query.metrics || '[]');
+        dimensions = JSON.parse(request.query.dimensions || '[]');
+        dateRanges = JSON.parse(request.query.dateRanges || '[]')
+        
+        const analyticsDataClient = new BetaAnalyticsDataClient({ auth });
+        const res = await analyticsDataClient.runReport({
+            property: `properties/${propertyId}`,
+            dateRanges: dateRanges,
+            dimensions: dimensions,
+            metrics: metrics,
+        });
+
+        response.json(res);
+
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({message: error})
+    }
+};
